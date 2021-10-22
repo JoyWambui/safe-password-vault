a5ebc53af7249a84fcbccd8206f176a7dc937230
JoyWambui <JoyWambui@github.com>
2021-10-22 22:22:00 +0300

    create and test method to save a single user

--- test_users_and_credentials.py
+++ test_users_and_credentials.py
@@ -9,9 +9,16 @@ class TestUsers(unittest.TestCase):
         
         self.new_user = User("minnie","password")
         
+        
     def test_init(self):
-        """Test to check whether the User object is being initialzed properly """
+        """Test to check whether the User object is being initialized properly """
         self.assertEqual(self.new_user.username,"minnie")
         self.assertEqual(self.new_user.password,"password")
+        
+    def test_save_user(self):
+        """Test that confirms a user account is being saved"""
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
+
 if __name__ == '__main__':
     unittest.main()
--- users_and_credentials.py
+++ users_and_credentials.py
@@ -1,10 +1,14 @@
 class User:
     """Class that generates new instances of users."""
+    users_list = []
     
     def __init__(self,username,password):
         """Initialize username and password attributes"""
         
         self.username = username
         self.password = password
+        
+    def save_user(self):
+        User.users_list.append(self)
     
     
